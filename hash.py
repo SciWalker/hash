3fb75fb8729878e724cee8c3b7e8209a112350da
ElijahWong <wongwenwei@live.com>
2020-07-11 22:07:35 +0800

    please accept

--- hash.py
+++ hash.py
@@ -24,7 +24,7 @@ def hash(block):
 
 
 ###################################################Start API######################################################
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
